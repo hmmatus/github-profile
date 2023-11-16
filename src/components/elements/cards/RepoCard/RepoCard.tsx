da386428bcbd32ea7b4937fb5c1e6d036613267f
Hector Matus <{ID}+{username}@users.noreply.github.com>
2023-11-16 17:24:43 -0600

    Added fetch data functionality, added Repo card

---
+++ src/components/elements/cards/RepoCard/RepoCard.tsx
@@ -0,0 +1,43 @@
+import { RepoI } from "@/models/repo.model";
+import Image from "next/image";
+import styles from "./RepoCard.module.scss";
+import { formatRelativeDate } from "@/utils/formatRelativeDate";
+type Props = {
+  repo: RepoI;
+}
+const RepoCard = ({
+  repo
+}: Props) => {
+  const {  name,
+    description,
+    forks,
+    watchers,
+    license,
+    updated_at} = repo;
+  const formattedDate = formatRelativeDate(updated_at);
+  return (
+    <div className={styles.container}>
+      <h4 className={styles['name-text']}>{name}</h4>
+      <p className={styles['description-text']}>{description}</p>
+      <div className={`${styles.flex} ${styles.row}`}>
+        {license && (
+          <div className={`${styles.flex} ${styles.ai} ${styles.jc}`}>
+            <Image width={30} height={30} alt="License" src={"/images/svg/license.svg"} />
+            <p className={styles['description-text']}>{license.key.toLocaleUpperCase()}</p>
+          </div>
+        )}
+        <div className={`${styles.flex} ${styles.ai} ${styles.jc} ${styles['mh-10']}`}>
+          <Image width={30} height={30} alt="Forks" src={"/images/svg/forks.svg"} />
+          <p className={`${styles['text-align-center']} ${styles['description-text']}`}>{forks}</p>
+        </div>
+        <div className={`${styles.flex} ${styles.ai} ${styles.jc}`}>
+          <Image width={30} height={30} alt="Forks" src={"/images/svg/star.svg"} />
+          <p className={styles['description-text']}>{watchers}</p>
+        </div>
+        <p className={styles['description-text']}>{formattedDate}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RepoCard;
--- src/components/layouts/home/HomeLayout.tsx
+++ src/components/layouts/home/HomeLayout.tsx
@@ -1,14 +1,56 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CustomInput from "@/components/elements/inputs/CustomInput/CustomInput";
 import styles from "./HomeLayout.module.scss";
 import InformationTag from "@/components/elements/tags/InformationTag/InformationTag";
+import { getProfile, getRepos } from "@/api/profile";
+import RepoCard from "@/components/elements/cards/RepoCard/RepoCard";
 
 const HomeLayout = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [profile, setProfile] = useState({});
+  const [repos, setRepos] = useState([]);
   const onChangeSearchValue = (value: string) => {
     setSearchValue(value);
   };
+  const [loading, setLoading] = useState(false);
+
+  async function fetchProfile(name: string) {
+    try {
+      const data = await getProfile(name);
+      setProfile(data);
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: HomeLayout.tsx:21 ~ fetchProfile ~ error:",
+        error
+      );
+    }
+  }
+  async function fetchRepos(name: string) {
+    try {
+      const data = await getRepos(name);
+      setRepos(data);
+    } catch (error) {
+      console.log("🚀 ~ file: HomeLayout.tsx:30 ~ fetchRepos ~ error:", error);
+    }
+  }
+
+  useEffect(() => {
+    const handleFetch = async () => {
+      setLoading(true);
+      await Promise.all([fetchProfile("github"), fetchRepos("github")]);
+      setLoading(false);
+    };
+    handleFetch();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className={styles.contianer}>
+        <h1>Loading</h1>
+      </div>
+    )
+  }
 
   return (
     <div className={styles.container}>
@@ -18,19 +60,31 @@ const HomeLayout = () => {
       <div className={styles["profile-header"]}>
         <div className={styles["image-container"]}>
           <img
-            src={"https://avatars.githubusercontent.com/u/9919?v=4"}
+            src={profile.avatar_url}
             className={styles["profile-img"]}
             alt="Profile Image"
           />
         </div>
         <div className={styles["tags-container"]}>
           <div className={styles.tags}>
-            <InformationTag label="Followers" value="123" />
-            <InformationTag label="Following" value="0" />
-            <InformationTag label="Location" value="San Francisto, CA" />
+            <InformationTag label="Followers" value={profile.followers} />
+            <InformationTag label="Following" value={profile.following} />
+            <InformationTag label="Location" value={profile.location} />
           </div>
         </div>
       </div>
+      <div className={styles["bio-container"]}>
+        <h1>{profile.name}</h1>
+        <p>{profile.bio}</p>
+      </div>
+      <div className={styles["repo-container"]}>
+        {repos.slice(0,3).map((elto, index) => (
+          <RepoCard repo={elto} key={index} />
+        ))}
+      </div>
+      <section>
+        <p>View all repositories</p>
+      </section>
     </div>
   );
 };
