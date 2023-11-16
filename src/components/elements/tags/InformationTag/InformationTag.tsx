6b9035dd6382d098ac9f25dab6aad330fb270c7b
Hector Matus <{ID}+{username}@users.noreply.github.com>
2023-11-16 14:57:04 -0600

    Added header section

---
+++ src/components/elements/tags/InformationTag/InformationTag.tsx
@@ -0,0 +1,16 @@
+import styles from './InformationTag.module.scss';
+type Props = {
+  label: string;
+  value: string;
+}
+const InformationTag = ({label, value}: Props) => {
+  return (
+    <div className={styles.container}>
+      <h5 className={styles.label}>{label}</h5>
+      <div className={styles.separator}></div>
+      <p className={styles.value}>{value}</p>
+    </div>
+  )
+}
+
+export default InformationTag;
--- src/components/layouts/home/HomeLayout.tsx
+++ src/components/layouts/home/HomeLayout.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import CustomInput from "@/components/elements/inputs/CustomInput/CustomInput";
 import styles from "./HomeLayout.module.scss";
+import InformationTag from "@/components/elements/tags/InformationTag/InformationTag";
 
 const HomeLayout = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -13,6 +14,22 @@ const HomeLayout = () => {
     <div className={styles.container}>
       <div className={styles.header}>
         <CustomInput value={searchValue} onChange={onChangeSearchValue} />
+      </div>
+      <div className={styles["profile-header"]}>
+        <div className={styles["image-container"]}>
+          <img
+            src={"https://avatars.githubusercontent.com/u/9919?v=4"}
+            className={styles["profile-img"]}
+            alt="Profile Image"
+          />
+        </div>
+        <div className={styles["tags-container"]}>
+          <div className={styles.tags}>
+            <InformationTag label="Followers" value="123" />
+            <InformationTag label="Following" value="0" />
+            <InformationTag label="Location" value="San Francisto, CA" />
+          </div>
+        </div>
       </div>
     </div>
   );
